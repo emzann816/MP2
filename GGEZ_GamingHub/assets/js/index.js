a0921305bc3806c9a7b30899b1ce6c5c4e4af466
emzann816 <142023271+emzann816@users.noreply.github.com>
2023-10-20 10:55:03 +0800

    update span icon and closemodal

--- GGEZ_GamingHub/assets/js/index.js
+++ GGEZ_GamingHub/assets/js/index.js
@@ -68,7 +68,7 @@ function logout(){
     <div class="modal-content">
       <div class="modal-header">
         <h4 class="modal-title">Logging-Out</h4>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" onclick="closeModal()" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <p> Are you sure, you want to Log-out?</p>
